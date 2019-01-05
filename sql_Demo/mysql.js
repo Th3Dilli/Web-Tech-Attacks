5d8c7dffc6814ab604c74944f36df992ccab69f6
Th3Dilli <dilli.1822@gmx.at>
2019-01-05 22:22:57 +0100

    added id showcase injection => prevention

--- sql_Demo/mysql.js
+++ sql_Demo/mysql.js
@@ -16,23 +16,24 @@ console.log(mysql.format(sql2,[value2_2]))
 // SELECT * from users where username = 'admin'
 // SELECT * from users where username = 'admin \' '
 
-let id = mysql.escapeId("users");
+let id1_1 = mysql.escapeId("users");
+let id1_2 = mysql.escapeId("users `Test ");
 let value3_1 = mysql.escape("admin");
 let value3_2 = mysql.escape("admin ' ");
-console.log("SELECT * from "+ id +" where username = "+ value3_1);
-console.log("SELECT * from "+ id +" where username = "+ value3_2);
+console.log("SELECT * from "+ id1_1 +" where username = "+ value3_1);
+console.log("SELECT * from "+ id1_2 +" where username = "+ value3_2);
 // SELECT * from `users` where username = 'admin'
-// SELECT * from `users` where username = 'admin \' '
+// SELECT * from `users ``Test ` where username = 'admin \' '
 
 let sql4 = "SELECT * from ?? where username = ?";
 console.log(mysql.format(sql4,["users","admin"]))
-console.log(mysql.format(sql4,["users","admin ' "]))
+console.log(mysql.format(sql4,["users `Test ","admin ' "]))
 // SELECT * from `users` where username = 'admin'
-// SELECT * from `users` where username = 'admin \' '
+// SELECT * from `users ``Test ` where username = 'admin \' '
 
 let db = mysql.createConnection(/* con details */);
 db.query("SELECT * from ?? where username = ?", ["users", "admin"], () => { /* do something */ });
 // query => SELECT * from `users` where username = 'admin'
 
-db.query("SELECT * from ?? where username = ?", ["users ' ", "admin ' "], () => { /* do something */ });
-// query => SELECT * from `users ' ` where username = 'admin \' '
+db.query("SELECT * from ?? where username = ?", ["users `Test ", "admin ' "], () => { /* do something */ });
+// query => SELECT * from `users ``Test ` where username = 'admin \' '
